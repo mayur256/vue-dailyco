5097e9310a56d5e40d0b1b0dae2be4fc96762d63
Mayur Upadhayay <mayur123a@gmail.com>
2024-05-11 17:09:59 +0530

    feat: Designed minimal ui for chat room entry form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+// Imports
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const handleFormSubmit = (event: Event) => {
+    const form = (event.target as HTMLFormElement);
+    const name = (form.elements as any).name.value || "";
+    
+
+    router.push({
+        path: "video-room",
+        query: { name }
+    })
+}
+
+</script>
+
 <template>
-  <main>
-    Home
-  </main>
+    <main class="container mt-4">
+        <p>Enter name to enter the room</p>
+        <form @submit.prevent="handleFormSubmit">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Name</label>
+                <input type="text" class="form-control" name="name" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </main>
 </template>
