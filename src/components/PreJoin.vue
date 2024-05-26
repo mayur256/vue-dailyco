f2bc8bfa6bb46419f121cf879c9d0432d1d4d769
Mayur Upadhayay <mayur123a@gmail.com>
2024-05-26 18:15:44 +0530

    feat: Implemented functionality to enumerate and change devices in Prejoin UI

--- src/components/PreJoin.vue
+++ src/components/PreJoin.vue
@@ -3,7 +3,7 @@
 
         <div class="card w-50">
             <div class="card-img-top">
-                
+
             </div>
             <div class="card-body">
 
@@ -19,10 +19,14 @@
                     <div class="col-md-12">
                         <label for="exampleFormControlInput1" class="form-label">Mic</label>
                         <select class="form-select" aria-label="Default select example">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                            <option
+                                v-for="mic in audioIn"
+                                :key="mic.deviceId"
+                                :value="mic.deviceId"
+                                @change="updateMic"
+                            >
+                                {{ mic.label }}
+                            </option>
                         </select>
                     </div>
                 </div>
@@ -31,10 +35,14 @@
                     <div class="col-md-12">
                         <label for="exampleFormControlInput1" class="form-label">Speaker</label>
                         <select class="form-select" aria-label="Default select example">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                            <option
+                                v-for="speaker in audioOut"
+                                :key="speaker.deviceId"
+                                :value="speaker.deviceId"
+                                @change="updateSpeaker"
+                            >
+                                {{ speaker.label }}
+                            </option>
                         </select>
                     </div>
                 </div>
@@ -43,10 +51,14 @@
                     <div class="col-md-12">
                         <label for="exampleFormControlInput1" class="form-label">Camera</label>
                         <select class="form-select" aria-label="Default select example">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                            <option
+                                v-for="camera in videoIn"
+                                :key="camera.deviceId"
+                                :value="camera.deviceId"
+                                @change="updateCamera"
+                            >
+                                {{ camera.label }}
+                            </option>
                         </select>
                     </div>
                 </div>
@@ -64,6 +76,7 @@
 </template>
 
 <script>
+import daily from "@daily-co/daily-js";
 
 export default {
     name: "PreJoin",
@@ -71,12 +84,22 @@ export default {
         return {
             userName: "",
             roomUrl: "",
+            callObject: null,
+            audioIn: [],
+            audioOut: [],
+            videoIn: []
         }
     },
     mounted() {
         this.userName = this.$route.query.name;
         this.roomUrl = this.$route.query.roomUrl;
 
+        // Create instance of Daily call object
+        const co = daily.createCallObject();
+        // Assign in data obj for future reference
+        this.callObject = co;
+
+        this.setupPrejoinScreen();
     },
     methods: {
         async initJoin() {
@@ -87,6 +110,48 @@ export default {
                     appState: "request_access"
                 }
             });
+        },
+
+        async getDevices() {
+            try {
+                const audioIn = [];
+                const audioOut = [];
+                const videoIn = [];
+                const devicesArr = await this.callObject.enumerateDevices();
+                
+                for (const d of (devicesArr?.devices ?? [])) {
+                    if (d?.kind === 'audioinput') {
+                        audioIn.push(d);
+                    } else if (d?.kind === 'audiooutput') {
+                        audioOut.push(d);
+                    } else if (d?.kind === 'videoinput') {
+                        videoIn.push(d);
+                    }
+                }
+
+                this.audioIn = audioIn;
+                this.audioOut = audioOut;
+                this.videoIn = videoIn;
+            } catch (error) {
+                // handle error
+                console.log(error)
+            }
+        },
+
+        setupPrejoinScreen() {
+            this.getDevices();
+        },
+
+        updateMic(e) {
+            this.callObject.setInputDevicesAsync({ audioDeviceId: e.target.value });
+        },
+
+        updateSpeaker(e) {
+            this.callObject.setOutputDevice({ audioDeviceId: e.target.value });
+        },
+
+        updateCamera(e) {
+            this.callObject.setInputDevicesAsync({ videoDeviceId: e.target.value });
         }
     }
 }
